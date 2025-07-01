3ab6bfa6be4f9a5723dea79f901c0202294e35f2
drafanasa <ducnh.0406@gmail.com>
2025-07-01 20:58:56 +0700

    Add files via upload

---
+++ img.zing.vn/products/vendor/general/360game/gn360.js
@@ -0,0 +1,11 @@
+var config360game = {
+  zing_account: "",
+  app_name: "gunny360",
+  show_header: 1
+};
+$(document).ready(function() {
+  try {
+    vmas_click("gunny360","mainsite360","");
+  }
+  catch(err) {}
+});
---
+++ img.zing.vn/products/vendor/general/call-function/call-function-gn.js
@@ -0,0 +1,13 @@
+var productCode = "gunny360"; // VMAS
+var arrayBanner = ['1'];
+var arrayBigBanner = [];
+var CodeProduct = "gn"; //
+var CookieProduct = "gn_Cookie";
+var CookieBottomProduct = "gn_CookieBottom";
+// initTopbarFlatForm("topbar", productCode, arrayBanner, arrayBigBanner); //apply top-bar
+var MASHTTPSource ="";
+var MASHTTPListCustomSource = [
+  {
+    
+  }
+];
